2bbfc860d356b5b2febf531f285bf1a1b614adbf
firecg12 <1lawalhan@gmail.com>
2025-09-18 19:04:24 -0400

    Git2code

--- Intro.py
+++ Intro.py
@@ -1,3 +1,3 @@
-#using print statement
+#using print statement 1st class
 print("Hello, World!")
-print("My name is Hanaan.")
+#print("My name is Hanaan.")
---
+++ secondclass.py
@@ -0,0 +1,9 @@
+
+x = 20
+name= "Hanaan"
+print(x)
+print(name)
+num1 = input("Enter your num1: ")
+print("The first number entered is", num1)
+num2 = input("Enter number num2: ")
+print("The second number entered is", num2)
