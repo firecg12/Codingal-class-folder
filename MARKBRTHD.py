ee97450d7c6aea08012f7370e10c6f8d1ed2d196
Firecgg <1lawalhan@gmail.com>
2025-11-20 19:08:56 -0500

    Refactor age input method using eval

--- MARKBRTHD.py
+++ MARKBRTHD.py
@@ -2,7 +2,8 @@
 print("Welcome to Marks Birthday Party!")
 print("Please provide the following details and you need to be 12 to 15 to enter:")
 try:
-    age = int(input("Enter your age: "))
+    #age = int(input("Enter your age: "))
+    age = eval(input("Enter your age: "))
     if 12 <= age <= 15:
         name = input("Enter your name: ")
         print(f"Hello {name}, you are eligible to enter the party!")
@@ -12,3 +13,4 @@ except SyntaxError:
     print("Invalid input. Please enter a valid age.")
 finally:
     print("Thank you for your interest!")
+
