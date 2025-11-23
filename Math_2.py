f11aae80f72d93e5e7042be5679e59a9dc025dac
Firecgg <1lawalhan@gmail.com>
2025-11-23 12:54:45 -0500

    Add files via upload

---
+++ Math_2.py
@@ -0,0 +1,7 @@
+import math
+
+x = int(input("Enter a number to find the sin cos and tan values: "))
+
+print(f"The sine value of {x} is: " + str(math.sin(x)))
+print(f"The cosine value of {x} is: " +  str(math.cos(x)))
+print(f"The tangent value of {x} is: " + str(math.tan(x)))
