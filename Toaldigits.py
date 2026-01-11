8348f13e2d380ee1d1e38da69b3d9305e08b2818
Firecgg <1lawalhan@gmail.com>
2026-01-11 12:37:24 -0500

    Add files via upload

---
+++ Toaldigits.py
@@ -0,0 +1,8 @@
+# creat a class
+class student:
+    # create a Variable grade
+    grade = 12
+    # print a scentence inside the class
+    print("Welcome to the class!")
+# create an object of the class
+obj = student()
