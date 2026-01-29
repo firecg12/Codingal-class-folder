6d86004f7673ea8f59b5366da589df8afc333dc0
Firecgg <1lawalhan@gmail.com>
2026-01-29 18:37:42 -0500

    Add files via upload

---
+++ computerpRiCe.py
@@ -0,0 +1,20 @@
+
+class ComputerPrice:
+
+    def __init__(self):
+        self.__maxprice = 900
+
+    def sell(self):
+        print("seling Price: {}".format(self.__maxprice))
+    
+    def setMaxPrice(self, price):
+        self.__maxprice = price 
+
+c = ComputerPrice()
+c.sell()
+
+c.__maxprice = 1000
+c.sell()
+
+c.setMaxPrice(1000)
+c.sell()
