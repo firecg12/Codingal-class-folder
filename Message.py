6e1d4735dbeb3f5f58ae3e4f307bae94ffd75443
Firecgg <1lawalhan@gmail.com>
2026-03-12 19:05:01 -0400

    Add files via upload

---
+++ Message.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter import messagebox
+
+# Setup Tkinter Window
+root = Tk()
+root.geometry("200x200")
+
+
+def msg():
+    messagebox.showwarning("Alert", "Stop! Virus Found.")
+
+# Adding Button Widget to Window
+button = Button(root, text="Scan for Virus", command=msg)
+button.place(x=40, y=80)
+
+# Entering main event loop
+root.mainloop()
