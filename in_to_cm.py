1a0274bc32ea6228dbbff2c6770cf23d91d0efe6
Firecgg <1lawalhan@gmail.com>
2026-03-14 20:43:39 -0400

    Add files via upload

---
+++ in_to_cm.py
@@ -0,0 +1,31 @@
+# converting inches to centimeters using Tkinter
+from tkinter import *
+
+root = Tk()
+root.title("Inches to Centimeters")
+root.geometry("600x400")
+
+frame = Frame(master=root, height=200, width=360, bg='lightblue')
+frame.place(x=120, y=50)
+
+lbl = Label(frame, text="Enter Measurement (in)", bg="#3895D3", fg="white")
+lbl.place(x=20, y=20)
+
+user_entry = Entry(frame)
+user_entry.place(x=180, y=20)
+
+cm = 2.54
+
+textbox = Text(root, bg="#000000", fg="white", height=5, width=40)
+textbox.place(x=120, y=200)
+
+def logic():
+    user = float(user_entry.get())   # convert text to number
+    calculation = user * cm
+    conversion = str(user) + " in is equal to " + str(calculation) + " cm\n"
+    textbox.insert(END, conversion)
+
+btn = Button(root, text="Convert", command=logic, bg="red")
+btn.place(x=250, y=150)
+
+root.mainloop()
